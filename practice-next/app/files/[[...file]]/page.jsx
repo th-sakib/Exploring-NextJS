ece8d8cdc618aa994803d40a8f271a37a49de027
th-sakib <mdthamidulhasansakib@gmail.com>
2025-02-06 01:08:38 +0600

    Making the catch all route optional - using [[]] make the route optional

--- practice-next/app/files/[[...file]]/page.jsx
+++ practice-next/app/files/[[...file]]/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 async function File({ params }) {
   const { file } = await params;
-  return <div>file path: /{file.join("/")}</div>;
+  return <div>file path: /{file?.join("/")}</div>;
 }
 
 export default File;
